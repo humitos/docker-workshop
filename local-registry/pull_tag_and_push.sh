279abc299e4c72be71861b7eb4a17f320b330983
Manuel Kaufmann <humitos@gmail.com>
2017-11-26 19:48:12 -0500

    Script for pull, tag and push automatically

---
+++ local-registry/pull_tag_and_push.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# https://github.com/docker-oxford/local-registry/blob/master/pull_tag_and_push.sh
+
+function usage() {
+  echo "usage: $0 [registry fqdn] [images]" && exit 1
+}
+[ "${1}" == "" ] && usage
+REGISTRY="${1}"
+# Define images
+shift
+HUB_IMAGES="${@}"
+echo ${HUB_IMAGES}
+# Pull all images from Docker Hub
+echo ${HUB_IMAGES} | xargs -I% -n1 docker pull %
+[ $? -ne 0 ] && echo "failed to fetch all images" && exit 1
+# Tag all images with local private registry
+echo ${HUB_IMAGES} | xargs -I% -n1 docker tag % ${REGISTRY}/%
+[ $? -ne 0 ] && echo "failed to tag all images" && exit 1
+# Push images to private registry
+echo ${HUB_IMAGES} | xargs -I% -n1 docker push ${REGISTRY}/%
+[ $? -ne 0 ] && echo "failed to push all images" && exit 1
+exit 0
